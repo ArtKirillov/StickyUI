0b706cdc590692fa6e1f27d35cf495b9a5c659a3
Artem Kirillov <artemkirillov@gmail.com>
2016-12-29 14:34:13 +0400

    Adds DoubleAlignmentLabel playground

---
+++ DoubleAlignmentLabel.playground/Contents.swift
@@ -0,0 +1,20 @@
+//
+//  Example of using DoubleAlignmentLabel
+//
+
+import UIKit
+
+let dictionary = [
+    "Key 1": "10",
+    "Key 2": "9",
+    "Key 3": "8",
+    "Key 4": "7",
+    "Key 5": "6",
+    "Key 6": "5",
+    "Key 7": "5"
+]
+let label = DoubleAlignmentLabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+label.set(text: dictionary)
+
+
+
